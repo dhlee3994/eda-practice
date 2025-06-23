6cca6fba7881f9480fd71185607ca9db11742a12
dhlee3994 <dhlee3994@gmail.com>
2025-06-23 12:10:45 +0900

    Initial commit

---
+++ src/main/kotlin/me/dhlee/edapractice/EdaPracticeApplication.kt
@@ -0,0 +1,11 @@
+package me.dhlee.edapractice
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class EdaPracticeApplication
+
+fun main(args: Array<String>) {
+    runApplication<EdaPracticeApplication>(*args)
+}
---
+++ src/test/kotlin/me/dhlee/edapractice/EdaPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package me.dhlee.edapractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class EdaPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
